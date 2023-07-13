bb97b35470341ff7e90608d7539d0ebe6273ded4
Riley-Naidoo <119883343+Riley-Naidoo@users.noreply.github.com>
2023-07-13 12:25:54 +0300

    Add files via upload

--- inverter.js
+++ inverter.js
@@ -1,71 +1,71 @@
 function inverter1(){
-  document.getElementById("product-button1").addEventListener("click", function() {
+  document.getElementById("product-button1").addEventListener("onclick", function() {
     window.location.href = "inverter-1.html";
   });
 }
 
 function inverter2(){
-  document.getElementById("product-button2").addEventListener("click", function() {
+  document.getElementById("product-button2").addEventListener("onclick", function() {
     window.location.href = "inverter-2.html";
   });
 }
 
 function inverter3(){
-  document.getElementById("product-button3").addEventListener("click", function() {
+  document.getElementById("product-button3").addEventListener("onclick", function() {
     window.location.href = "inverter-3.html";
   });
 }
 
 function inverter4(){
-  document.getElementById("product-button4").addEventListener("click", function() {
+  document.getElementById("product-button4").addEventListener("onclick", function() {
     window.location.href = "inverter-4.html";
   });
 }
 
 function inverter5(){
-  document.getElementById("product-button5").addEventListener("click", function() {
+  document.getElementById("product-button5").addEventListener("onclick", function() {
     window.location.href = "inverter-5.html";
   });
 }
 
 function inverter6(){
-  document.getElementById("product-button6").addEventListener("click", function() {
+  document.getElementById("product-button6").addEventListener("onclick", function() {
     window.location.href = "inverter-6.html";
   });
 }
 
 function inverter7(){
-  document.getElementById("product-button7").addEventListener("click", function() {
+  document.getElementById("product-button7").addEventListener("onclick", function() {
     window.location.href = "inverter-7.html";
   });
 }
 
 function inverter8(){
-  document.getElementById("product-button8").addEventListener("click", function() {
+  document.getElementById("product-button8").addEventListener("onclick", function() {
     window.location.href = "inverter-8.html";
   });
 }
 
 function inverter9(){
-  document.getElementById("product-button9").addEventListener("click", function() {
+  document.getElementById("product-button9").addEventListener("onclick", function() {
     window.location.href = "inverter-9.html";
   });
 }
 
 function inverter10(){
-  document.getElementById("product-button10").addEventListener("click", function() {
+  document.getElementById("product-button10").addEventListener("onclick", function() {
     window.location.href = "inverter-10.html";
   });
 }
 
 function inverter11(){
-  document.getElementById("product-button11").addEventListener("click", function() {
+  document.getElementById("product-button11").addEventListener("onclick", function() {
     window.location.href = "inverter-11.html";
   });
 }
 
 function inverter12(){
-  document.getElementById("product-button12").addEventListener("click", function() {
+  document.getElementById("product-button12").addEventListener("onclick", function() {
     window.location.href = "inverter-12.html";
   });
 }
