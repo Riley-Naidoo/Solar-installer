eed57504924881d62a520a8340f5a29d6ed95d16
Riley-Naidoo <119883343+Riley-Naidoo@users.noreply.github.com>
2023-07-13 15:00:43 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function gotoInverter(){
-    document.getElementById("button").addEventListener("click", function() {
+    document.getElementById("button-2").addEventListener("click", function() {
       window.location.href = "inverter.html";
     });
   }
 
   function gotoBattery(){
-    document.getElementById("button").addEventListener("click", function() {
+    document.getElementById("button-3").addEventListener("click", function() {
       window.location.href = "battery.html";
     });
   }
 
   function gotoPanel(){
-    document.getElementById("button").addEventListener("click", function() {
+    document.getElementById("button-4").addEventListener("click", function() {
       window.location.href = "panel.html";
     });
   }
 
   function gotoCalc(){
-    document.getElementById("button").addEventListener("click", function() {
+    document.getElementById("button-1").addEventListener("click", function() {
       window.location.href = "calc.html";
     });
   }
